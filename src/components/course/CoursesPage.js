c270ebcbaae1b5c561b388a941ea8ad3b469bfcc
Bernard <bernard@plantminer.com.au>
2018-03-14 13:19:03 +0800

    added new files

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,57 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseAction from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
+  constructor(props, context){
+    super(props, context);
+
+    this.state = {
+      course: { title: '' }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event){
+    const course = this.state.course;
+    course.tile = event.target.value;
+    this.setState({
+      course: course
+    });
+  }
+
+  onClickSave(){
+    this.props.dispatch(courseAction.createCourse(this.state.course));
+  }
+
   render(){
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange} />
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
       </div>
     );
   }
 }
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps){
+  return {
+    courses: state.courses
+  };
+}
+
+CoursesPage.propTypes = {
+  dispatch: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps)(CoursesPage);
