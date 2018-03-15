4e94ecd47918753f194e69e9394b2fd541a7ef11
Bernard <bernard@plantminer.com.au>
2018-03-15 16:01:26 +0800

    added thunks

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,8 +1,19 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
 export function createCourse(course){
   return {
     type: types.CREATE_COURSE,
     course
+  };
+}
+
+export function loadCourses(){
+  return function(dispatch){
+    return courseApi.getAllCourses().then(course => {
+      dispatch(loadCoursesSuccess(course));
+    }).catch(error => {
+      throw(error);
+    })
   };
 }
